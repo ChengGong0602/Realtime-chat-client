edf0444574a56f9e9edda6038c7145a2e3fe8b04
ChengGong0602 <rjs0602@163.com>
2021-11-18 07:27:51 +0800

    update database integration

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import queryString from 'query-string';
 import io from "socket.io-client";
+import axios from 'axios';
+import moment from 'moment';
 
 import TextContainer from '../TextContainer/TextContainer';
 import Messages from '../Messages/Messages';
@@ -9,20 +11,26 @@ import Input from '../Input/Input';
 
 import './Chat.css';
 
-const ENDPOINT = 'https://react-socket-server-cheng.herokuapp.com';
-// const ENDPOINT = "http://localhost:5000";
+// const ENDPOINT = 'https://react-socket-server-cheng.herokuapp.com';
+const ENDPOINT = "http://localhost:5000";
 
 let socket;
 
 const Chat = ({ location }) => {
   const [name, setName] = useState('');
+  const [time, setTime] = useState('');
   const [room, setRoom] = useState('');
   const [users, setUsers] = useState('');
+  const [message_array, setMessage_array] = useState([]);
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
+  
+  
 
   useEffect(() => {
+
     const { name, room } = queryString.parse(location.search);
+    console.log("location.search===",location.search)
 
     socket = io(ENDPOINT);
 
@@ -37,8 +45,35 @@ const Chat = ({ location }) => {
   }, [ENDPOINT, location.search]);
   
   useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+    const params = { room_name : room };   
+
+    axios.post(ENDPOINT +'/roomData', params)
+        .then(response => {
+          let tempArr = response.data.response
+          let newArr = []
+          for (let i = 0; i < tempArr.length; i++) {
+            let newObj = {}
+            newObj.user = tempArr[i].user_name
+            newObj.text = tempArr[i].message
+            newObj.time = moment(tempArr[i].created_at).format('h:mm a')
+            newArr.push(newObj)
+          }
+          setMessages(newArr)
+        });
+
+
+
+        // {id: 6, user_name: 'tom', room_name: 'testroom', message: 'hujgj', created_at: 1624717576000}
+// 1: {id: 7, user_name: 'tom', room_name: 'testroom', message: 'hjjghj', created_at: 1624717576000}
+// 2: {id: 8, user_name: 'tom', room_name: 'testroom', message: 'jhj', created_at: 1624717576000}
+// 3: {id: 9, user_name: 'tom', room_name: 'testroom', message: 'jhjhjhjhjhjhj', created_at: 1624717576000}
+// 4: {id: 10, user_name: 'tom', room_name: 'testroom', message: 'hi', created_at: 1624717576000}
+// 5: {id: 11, user_name: 'jones', room_name: 'testroom', message: 'hello', created_at: 1624717576000}
     socket.on('message', message => {
       setMessages(messages => [ ...messages, message ]);
+      console.log("messages==>", messages)
+      console.log("message-----", message)
     });
     
     socket.on("roomData", ({ users }) => {
@@ -58,7 +93,7 @@ const Chat = ({ location }) => {
     <div className="outerContainer">
       <div className="container">
           <InfoBar room={room} />
-          <Messages messages={messages} name={name} />
+          <Messages messages={messages} name={name} time={time} />
           <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
       </div>
       <TextContainer users={users}/>
